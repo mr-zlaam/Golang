7b44475634bb9b17a513b1ba1ad995580facb9dd
Zlaam <mrzalaam@gmail.com>
2024-07-12 18:28:43 +0500

    start learning golang

---
+++ 2_simpleValues/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Numbers
+	fmt.Println(1 + 1)
+	//Strings
+	fmt.Println("Hello" + "golang")
+	//Boolean
+	fmt.Println(true && false)
+	// Floats
+	fmt.Println(10.5)
+}
---
+++ 2_variables/variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hello string = "hello world"
+	fmt.Println(hello)
+	// another way to declare variable
+	name := "hello world"
+	fmt.Println(name)
+}
